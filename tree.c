a529cc62f659510c252b85dd33776dcdaf5d7cb8
Vincent Andrieu <vandrieu@localhost.localdomain>
2019-10-04 19:46:19 +0200

    fix spaces

--- tree.c
+++ tree.c
@@ -17,7 +17,7 @@ static void print_x_characters(int x, char c)
 static void print_trunk(int size, int zoom)
 {
     for (int y = 1; y <= size; y++) {
-        print_x_characters(size * zoom - size / 2, ' ');
+        print_x_characters((size - 2) * 4 + 12 - size / 2, ' ');
         print_x_characters(size, '|');
         my_putchar('\n');
     }
@@ -27,7 +27,7 @@ static int print_floor(int size, int floor, int top_stars, int num_spaces)
 {
     int step;
 
-    for (step = top_stars; step <= (floor + 4 + top_stars); step++) {
+    for (step = top_stars; step < (floor + 4 + top_stars); step++) {
         print_x_characters(num_spaces, ' ');
         print_x_characters(1 + step * 2, '*');
         num_spaces--;
@@ -38,27 +38,18 @@ static int print_floor(int size, int floor, int top_stars, int num_spaces)
 
 void tree(int size)
 {
-    print_x_characters(1, '\n');
-    int zoom = 4;
-    if (size > 2) {
-        zoom = 5;
-        if (size > 5) {
-            zoom = size;
-        }
-    }
     int top_stars = 0;
-    int num_spaces = size * zoom;
+    int num_spaces = (size - 2) * 4 + 12;
 
     for (int floor = 0; floor < size; floor++) {
         top_stars = print_floor(size, floor, top_stars, num_spaces);
-        num_spaces  = (size * zoom) - (top_stars);
+        num_spaces  = ((size - 2) * 4 + 12) - (top_stars);
     }
-    print_trunk(size, zoom);
-    print_x_characters(2, '\n');
+    print_trunk(size, (size - 2) * 4 + 12);
 }
-
 
 //Etage du dessus = 2/3 * Etage
 //Etage du dessous = 2/3 / Etage
 //Espaces en haut avant première étoile = 5*4 - 1
 //Dernière ligne avant tronc = size*4
+//v_ligne = (size - 2) * 8 + 13
